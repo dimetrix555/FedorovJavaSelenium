ae719beb56503ee8d29ba09d36a7fb8ad3a4c6ea
dimetrix555 <dimetrix55@gmail.com>
2021-10-11 20:42:20 +0300

    Inheritance example

---
+++ src/mypackage/Inheritance.java
@@ -0,0 +1,24 @@
+package mypackage;
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+
+        cat.name = "Vasya";
+        dog.name = "Barsik";
+
+        cat.color = "White";
+        dog.color = "Black";
+
+        cat.walk("Backyard");
+        dog.walk("Park");
+
+        cat.feed("Milk");
+        dog.sit();
+
+        System.out.println("Cat name is: " + cat.name);
+        System.out.println("Dog name is: " + dog.name);
+    }
+}
